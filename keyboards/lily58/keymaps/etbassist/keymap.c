dbece7ac0718455ac6cfeedea98f7d365509c689
Eugene Theriault <hybridbassist@gmail.com>
2021-02-15 17:22:49 -0800

    Add additional symbols to _LOWER layout

--- keyboards/lily58/keymaps/etbassist/keymap.c
+++ keyboards/lily58/keymaps/etbassist/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_ENT, KC_SPC, MO(_RAISE), KC_BSPC, KC_RALT \
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_ENT, KC_SPC, MO(_RAISE), C(A(KC_LGUI)), KC_RALT \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------| Left | Down |  Up  |Right |   )  |   -  |
  * |------+------+------+------+------+------|   [   |    |    )  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |   ^  |   &  |   *  |   (  |   )  |   ~  |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, KC_RPRN, KC_TILD, \
-  _______, _______, _______, _______, _______, _______, _______, KC_RPRN, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, _______, KC_RPRN, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |C-Lock|  F2  |   1  |   2  |   3  |   *  |-------.    ,-------| Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|   (   |    |    ]  |------+------+------+------+------+------|
- * |  F7  |  F8  |   0  |   +  |   -  |   =  |-------|    |-------|   ^  |   $  |   =  |   [  |   ]  |   \  |
+ * |LShift|  F8  |   0  |   +  |   -  |   =  |-------|    |-------|   ^  |   $  |   =  |   [  |   ]  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_7,    KC_8,    KC_9, _______,                     _______, _______, _______, _______, _______, _______, \
   KC_GRV,  KC_1,    KC_4,    KC_5,    KC_6,   KC_PSLS,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_CAPS, KC_F2,   KC_1,    KC_2 ,   KC_3 ,  KC_PAST,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,_______, XXXXXXX, \
-  _______, KC_F8,   KC_0 ,   KC_PLUS, KC_MINS, KC_EQL,   KC_LPRN, _______,  KC_CIRC, KC_DLR , KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, KC_F8,   KC_0 ,   KC_PLUS, KC_MINS, KC_EQL,   KC_LPRN, _______,  KC_CIRC, KC_DLR , KC_EQL,  KC_LBRC, KC_RBRC, _______, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
